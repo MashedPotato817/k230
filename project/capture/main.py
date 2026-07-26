048b2082a77d6c87341b91b3b7047149b664d870
MashedPotato817 <hangningzhang8@gmail.com>
2026-07-26 20:07:59 +0800

    feat: 更新超广角摄像头配置，修改保存路径为/data/balls，并添加照片编号显示

--- project/capture/main.py
+++ project/capture/main.py
@@ -2,16 +2,16 @@
 # @file         main.py
 # @brief        超广角摄像头拍照
 #
-# 使用J2/CSI1超广角摄像头。K0拍照并保存到/data/pic。
+# 使用J2/CSI2超广角摄像头。K0拍照并保存到/data/balls。
 #####################################################################################################
 
 import time
 import os
+import image
 from machine import Pin, FPIOA
 from media.sensor import *
 from media.display import *
 from media.media import *
-
 
 sensor = None
 
@@ -21,12 +21,12 @@ try:
     key0 = Pin(34, Pin.IN, pull=Pin.PULL_UP, drive=7)
 
     try:
-        os.mkdir("/data/pic")
+        os.mkdir("/data/balls")
     except Exception:
         pass
 
-    # J2接口的超广角摄像头对应MIPI CSI1。
-    sensor = Sensor(id=1, width=1280, height=960)
+    # J2接口的超广角摄像头对应MIPI CSI2。
+    sensor = Sensor(id=2, width=1280, height=960)
     sensor.reset()
 
     # 通道0实时显示，通道1输出高分辨率照片。
@@ -38,20 +38,31 @@ try:
     bind_info = sensor.bind_info()
     Display.bind_layer(**bind_info, layer=Display.LAYER_VIDEO1)
     Display.init(Display.ST7701, width=640, height=480, to_ide=True)
+    osd_img = image.Image(640, 480, image.ARGB8888)
     MediaManager.init()
     sensor.run()
-    print("wide camera ready: CSI1")
+    print("wide camera ready: CSI2")
 
     key0_last = 1
+    last_photo_number = 0
     while True:
         os.exitpoint()
         key0_now = key0.value()
 
+        # 在预览画面左上角显示上一张已保存照片的编号。
+        osd_img.clear()
+        osd_img.draw_string_advanced(
+            10, 10, 24, "pic_z%06d" % last_photo_number, color=(255, 255, 255, 255)
+        )
+        Display.show_image(osd_img, 0, 0, Display.LAYER_OSD3)
+
         # K0下降沿拍照，长按只保存一张。
         if key0_last and not key0_now:
-            filename = "/data/pic/wide_%d.jpg" % time.ticks_ms()
+            photo_number = last_photo_number + 1
+            filename = "/data/balls/z%06d.jpg" % photo_number
             photo = sensor.snapshot(chn=CAM_CHN_ID_1)
             photo.save(filename, quality=95)
+            last_photo_number = photo_number
             print("photo saved:", filename)
 
         key0_last = key0_now
